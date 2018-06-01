e5ef3934e878a7d2a3182c104840caae59b32d18
Chris Grocott <chris.grocott@gmail.com>
2018-06-01 15:27:58 +0100

    further attempt to fix heroku

--- src/main/scala/example/server/Server.scala
+++ src/main/scala/example/server/Server.scala
@@ -9,13 +9,15 @@ import org.http4s.dsl.Http4sDsl
 import org.http4s.server.blaze.BlazeBuilder
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Properties
 
 object Server extends StreamApp[IO] with Http4sDsl[IO] {
 
-  val port: Int = Option(System.getProperty("PORT")).getOrElse("8080").toInt
+  //  val port: Int = Option(System.getProperty("PORT")).getOrElse("8080").toInt
+  val port = Properties.envOrElse("PORT", "8080").toInt
 
   val helloWorldService: HttpService[IO] = HttpService[IO] {
-      ChatRoomRoutes.routes()
+    ChatRoomRoutes.routes()
   }
 
   override def stream(args: List[String], requestShutdown: IO[Unit]): fs2.Stream[IO, ExitCode] =
